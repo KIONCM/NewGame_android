00e629d9a034741eb3f51ecac6dde5398ead2a4f
ahmed <ahmed.mostfa.ghorab@gmail.com>
2021-07-28 03:53:35 +0200

    register api

--- app/src/main/java/com/example/game/api/WebServiceDAO.kt
+++ app/src/main/java/com/example/game/api/WebServiceDAO.kt
@@ -4,6 +4,7 @@ import com.example.game.pojo.models.User
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.Single
+import retrofit2.Call
 import retrofit2.http.*
 
 interface WebServiceDAO {
@@ -11,4 +12,7 @@ interface WebServiceDAO {
 //@Headers("accept: */*","Content-Type: application/json")
     @POST("/api/Account")
     fun register(@Body model: HashMap<String,String>): Observable<Response>
+
+    @POST("/api/Account")
+    fun register2(@Body model: HashMap<String,String>): Call<Response>
 }
